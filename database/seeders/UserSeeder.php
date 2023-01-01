42e52553ab03e784684f17db3aca9acbaa60415c
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-01-01 21:31:53 +0800

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Customer Service Executive',
+                'email'          => 'cse@mail.com',
+                'phone_no'       => '0193822850',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'Vet',
+                'email'          => 'vet@mail.com',
+                'phone_no'       => '0122500064',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 3,
+                'name'           => 'Vet Asisstant',
+                'email'          => 'vetassistant@mail.com',
+                'phone_no'       => '01137627101',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 4,
+                'name'           => 'Client',
+                'email'          => 'client@mail.com',
+                'phone_no'       => '017456342',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
