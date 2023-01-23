e407d6bfbb0dc1c9bb40e406fa5ff44c2e94459f
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-01-24 01:21:12 +0800

    design login page

--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -5,8 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Add Patient') }}</div>
-
                 <div class="card-body">
                   {{-- @if (session('status'))
                       <div class="alert alert-success" role="alert">
