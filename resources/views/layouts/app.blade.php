eeadde53a43ae567e4ebaeb0953aadc4d8f2597c
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-02-03 13:41:52 +0800

    move nav to different blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,26 +41,7 @@
                         
                     @else
                     <ul class="navbar-nav me-auto">
-                        @can('patient_access')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('patients.index') }}">{{ __('Patients') }}</a>
-                             </li>
-                        @endcan
-                        @can('role_access')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('roles.index') }}">{{ __('Roles') }}</a>
-                            </li>
-                        @endcan
-                        @can('permission_access')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('permissions.index') }}">{{ __('Permissons') }}</a>
-                            </li>
-                        @endcan
-                        @can('user_access')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('users.index') }}">{{ __('Users') }}</a>
-                            </li>
-                        @endcan
+                        @include('layouts.nav')
                     </ul>
                     @endguest
 
