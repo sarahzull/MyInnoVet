e9431e37e75faeb56e250ac8317b4dd7483a580c
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-05-31 19:57:39 +0800

    Revert "update admin dashboard"
    This reverts commit 0e886cbe91be26d77804f8f52bc41b09d4747d65.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,6 +17,29 @@ class DashboardController extends Controller
         })->count();
         $totalPatients = Patient::count();
 
-        return view('pages.dashboards.index', compact('totalClients', 'totalPatients'));
+        $totalVets = User::whereHas('roles', function ($query) {
+            $query->where('name', 'Veterinarian');
+        })->count();
+
+        $todayAppointments = Appointment::with(['slots', 'slots.slotDetails'])
+        ->join('slots', 'appointments.slot_id', '=', 'slots.id')
+        ->whereDate('slots.date', now()->format('Y-m-d')) // Filter appointments for today's date
+        ->count();
+
+        $todayRegisteredPatients = Patient::whereDate('created_at', now()->toDateString())->count();
+
+        $today = Carbon::now()->format('d F Y');
+
+        $todayPatients = Patient::whereDate('created_at', now()->toDateString())->get();
+
+        return view('pages.dashboards.index', compact(
+            'totalClients', 
+            'totalPatients', 
+            'totalVets', 
+            'todayAppointments', 
+            'todayRegisteredPatients', 
+            'today', 
+            'todayPatients',
+        ));
     }
 }
