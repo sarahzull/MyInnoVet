a2ea769b21b59c107fe43cfa63c1db8f92a0b96d
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-06-01 11:20:14 +0800

    fix delete appointment

--- app/Http/Livewire/Pages/Appointments/Index.php
+++ app/Http/Livewire/Pages/Appointments/Index.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Livewire\Pages\Appointments;
 
-use App\Models\Appointment;
-use App\Models\RefTimeSlot;
 use App\Models\Slot;
 use Livewire\Component;
+use App\Models\Appointment;
+use App\Models\RefTimeSlot;
+use App\Services\TwilioService;
 
 class Index extends Component
 {
+    protected $listeners = ['confirmDeleteAppointment'];
+
     public function mount()
     {
         $todaySlot = Slot::whereDate('date', now()->format('Y-m-d'))->get();
@@ -30,10 +33,31 @@ class Index extends Component
         $this->emit('showDeleteConfirmation', $id);
     }
 
-    public function confirmDeleteAppointment($id)
+    public function confirmDeleteAppointment(TwilioService $twilioService, $id)
     {
-        $apt = Appointment::find($id);
-        $apt->slots->update(['status' => 0]);
+        $appointment = Appointment::with(['patient', 'staffs', 'slots', 'slots.slotDetails'])
+                            ->join('slots', 'appointments.slot_id', '=', 'slots.id')
+                            ->where('appointments.id', $id)
+                            ->select('appointments.*')
+                            ->first();
+
+        if (!$appointment) {
+            session()->flash('success', 'Appoinment doesnt exist.');
+            return redirect()->route('appointments.index');
+        }
+
+        $ownerName = $appointment->patient->owner->name;
+        $petName = $appointment->patient->name;
+
+        $appointment->slots->update(['status' => 0]);
+
+        $bodyMessage = "Hello *$ownerName*,\n\n";
+        $bodyMessage .= "We regret to inform you that your pet *$petName* appointment has been canceled.\n";
+        $bodyMessage .= "If you have any questions or would like to reschedule, please feel free to contact us.\n\n";
+        $bodyMessage .= "Thank you for considering MyInnoVet for your veterinary services.";
+
+        $twilioService->sendWhatsAppMessage($bodyMessage);
+
         Appointment::whereId($id)->delete();
 
         session()->flash('success', 'Appoinment deleted successfully.');
